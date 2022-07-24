dcd9cad6b062dee0c531604d953b2c41e5bf9ea3
xlx <863134326@qq.com>
2022-07-24 21:08:56 +0800

    打完打完哇我阿德爱爱丸

--- C910_RTL_FACTORY/gen_rtl/idu/rtl/ct_idu_id_split_long.v
+++ C910_RTL_FACTORY/gen_rtl/idu/rtl/ct_idu_id_split_long.v
@@ -12,7 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+*/git config --global http.proxy
 
 // &ModuleBeg; @24
 module ct_idu_id_split_long(
